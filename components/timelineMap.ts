42e3c1e86a373b2265f1fddecf8ebc1e19229a89
michellevit <mvflandin@gmail.com>
2025-05-15 21:07:56 -0700

    Add sprite images

--- components/timelineMap.ts
+++ components/timelineMap.ts
@@ -3,12 +3,18 @@
 // Note: The song 'Fennec Takes Charge' is 1m30s
 
 export const spriteTimeline = [
-  { time: 1, src: "/sprite/sprite-emerge-1.png" },
-  { time: 2, src: "/sprite/sprite-emerge-2.png" },
-  { time: 3, src: "/sprite/sprite-emerge-3.png" },
-  { time: 4, src: "/sprite/sprite-run-1.png" },
-  { time: 4.3, src: "/sprite/sprite-run-2.png" },
-  { time: 4.6, src: "/sprite/sprite-run-1.png" },
-  { time: 4.9, src: "/sprite/sprite-run-2.png" },
-  { time: 5.2, src: "/sprite/sprite-run-1.png" },
+  { time: 1, src: "/sprite/start1.png" },
+  { time: 1.5, src: "/sprite/start2.png" },
+  { time: 2, src: "/sprite/run1.png" },
+  { time: 2.5, src: "/sprite/run2.png" },
+  { time: 3, src: "/sprite/run1.png" },
+  { time: 3.5, src: "/sprite/run2.png" },
+  { time: 4, src: "/sprite/run1.png" },
+  { time: 4.5, src: "/sprite/run2.png" },
+  { time: 5, src: "/sprite/run1.png" },
+  { time: 5.5, src: "/sprite/jump1.png" },
+  { time: 6, src: "/sprite/jump2.png" },
+  { time: 6.5, src: "/sprite/jump3.png" },
+  { time: 7, src: "/sprite/jump4.png" },
+  { time: 7.5, src: "/sprite/jump5.png" },
 ];
